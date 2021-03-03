517b253aa4968930455f11cf5766793316b63903
ocawthorne <ocawthorne@gmail.com>
2021-03-03 22:42:07 +0000

    Update to match original

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -81,7 +81,7 @@ export const checkLoggedIn = () => {
 }
 
 export const logout = () => {
-   return dispatch => {`${baseUrl}/api/v1/sessions`
+   return dispatch => {
       fetch(`${baseUrl}/api/v1/logout`, {
          method: "DELETE",
          credentials: "include"
